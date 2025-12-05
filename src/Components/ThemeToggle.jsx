dfe5056e8a969fd3868480efbed4bc842a5f8b21
amshuvo2002 <amshuvo.mail1425@gmail.com>
2025-12-05 22:42:22 +0600

    first commit

---
+++ src/Components/ThemeToggle.jsx
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from "react";
+
+const ThemeToggle = () => {
+  const [theme, setTheme] = useState(localStorage.getItem("theme") || "light");
+
+  useEffect(() => {
+    const root = document.documentElement;
+    root.setAttribute("data-theme", theme);
+    localStorage.setItem("theme", theme);
+  }, [theme]);
+
+  const handleToggle = (e) => {
+    setTheme(e.target.checked ? "dark" : "light");
+  };
+
+  return (
+    <label className="flex items-center bg-gray-100 rounded-xl gap-2 cursor-pointer ml-10">
+      <input
+        type="checkbox"
+        className="toggle theme-controller"
+        checked={theme === "dark"}
+        onChange={handleToggle}
+      />
+    </label>
+  );
+};
+
+export default ThemeToggle;
---
+++ src/main.jsx
@@ -0,0 +1,27 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+import MainLayout from './MainLayout/MainLayout.jsx';
+import Home from './Components/Home.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout></MainLayout>,
+    children: [
+        {
+        path: "/",
+        element: <Home></Home>
+      }
+    ]
+  },
+]);
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+     <RouterProvider router={router} />
+  </StrictMode>
+)
